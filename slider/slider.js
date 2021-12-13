370da264acfef15fa843957cf914f52abba687e1
Kim <kkaufhold@ensodata.app>
2021-12-12 21:00:36 -0800

    cleans up code a bit

--- slider/slider.js
+++ slider/slider.js
@@ -1,14 +1,5 @@
 // Make the slider icon element draggable:
-
 var button = document.getElementById("mySliderIcon");
-
-//function dragElement(elmnt) {
-  //var pos1 = 0, pos2 = 0;
-  //grab the element you are trying to move:
-  //if (document.getElementById("mySliderIcon")) {
-    //elmnt.onmousedown = dragMouseDown;
-  //}
-//}
 
 document.getElementById("mySliderIcon").onmousedown = dragMouseDown;
 
@@ -31,14 +22,15 @@ document.getElementById("mySliderIcon").onmousedown = dragMouseDown;
     console.log(pos1, pos2);
     if (pos2 < 90) {
       button.style.top = "90px";
-    } else if (pos2 > yConstraint) {
-      //what about if yConstraint doesn't exist yet/hasn't been set?
+    } else if (pos2 >= yConstraint) {
       button.style.top = yConstraint + "px";
     } else {
-      button.style.top = (button.offsetTop - pos1) + "px";
+      //button.style.top = (button.offsetTop - pos1) + "px";
+      button.style.top = pos2 + "px";
     } 
+    console.log(yConstraint);
   }
-
+  //set default maximum along y-axis
   let yConstraint = 191;
 
   function closeDragElement() {
@@ -47,35 +39,7 @@ document.getElementById("mySliderIcon").onmousedown = dragMouseDown;
     document.onmousemove = null;
   }
 
-// function applySliderConfigs(buttonColor, trackHeight, trackIntervals, defaultButtonPosition) {
-//   //set slider icon color
-//   document.getElementById("mySliderIcon").style.backgroundColor = buttonColor;
-
-//   //set slider track size
-//   document.getElementById("slider-track").style.height = trackHeight + "px";
- 
-//   //pass in an array of values to create slider scale
-//   //CLEAR EXISTING DIVS
-//   document.getElementById("labels-container").innerHTML = '';
-//   for (i=0;i<trackIntervals.length;i++) {
-//     var labelDiv = document.createElement("DIV");
-//     labelDiv.innerHTML = trackIntervals[i] + "&mdash;";
-//     document.getElementById("labels-container").appendChild(labelDiv);
-//   }
-//   //set default slider position
-//   document.getElementById("mySliderIcon").style.top = document.getElementById("mySliderIcon").style.top + defaultButtonPosition + "px";
-// }
-
-// function getSliderInput(event){
-//   const buttonColor = document.getElementById("color-input").value;
-//   const trackHeight = document.getElementById("track-height-input").value;
-//   const trackIntervals = document.getElementById("track-intervals-input").value;
-//   const defaultButtonPosition = document.getElementById("default-position-input").value;
-
-//   console.log(buttonColor, trackHeight, trackIntervals, defaultButtonPosition);
-//   applySliderConfigs(buttonColor, trackHeight, trackIntervals, defaultButtonPosition);
-// }
-
+//handle submit ---> allow user to set slider configurations
 document.getElementById('submit-button')
   .addEventListener('click', (e) => {
     e.preventDefault();
@@ -87,9 +51,12 @@ document.getElementById('submit-button')
     
       //set slider track size
       document.getElementById("slider-track").style.height = trackHeight + "px";
+      document.getElementById("labels-container").style.height = trackHeight + "px";
+      yConstraint = document.getElementById("track-height-input").value;
      
-      //pass in an array of values to create slider scale
+      
       //CLEAR EXISTING DIVS
+      //pass in values to create slider scale
       document.getElementById("labels-container").innerHTML = '';
       for (i=0;i<trackIntervals.length;i++) {
         var labelDiv = document.createElement("DIV");
@@ -106,15 +73,9 @@ document.getElementById('submit-button')
     const trackIntervals = [];
     for (i=0; i<document.getElementById("track-intervals-input").value.length; i++) {
       trackIntervals.push(document.getElementById("track-intervals-input").value[i]);
-      //var labelDiv = document.createElement("DIV");
-      //labelDiv.innerHTML = trackIntervals[i] + "&mdash;";
-      //document.getElementById("labels-container").appendChild(labelDiv);
     }
 
     const trackHeight = document.getElementById("track-height-input").value;
-    yConstraint = document.getElementById("track-height-input").value;
-    document.getElementById("labels-container").style.height = trackHeight;
-    document.getElementById("flex-container-1").style.height = trackHeight;
 
     const defaultButtonPosition = document.getElementById("default-position-input").value;
 
@@ -126,15 +87,8 @@ document.getElementById('submit-button')
 
 //current bugs:
 
-//2. labels do not spread out to fill whole container
-//   (try altering the space of labels-container a la Naya)
-//   "How can they pick up the same height automatically?" height 100%...
-
 
 //3.   slider button does not slide throughout entire track height
-
-
-//4.  Matching slider constraints to height of slider
 
 //5. not able to pass multididgit labels to intervals-writer
 //   can we work on passing in an actual array?
